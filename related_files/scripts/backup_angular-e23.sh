f0aaf2ea3015c11a3a44dc59b981509ef4643b53
gcoutu <gc8395@gmail.com>
2025-02-25 19:11:07 -0500

    Exercice Angular

--- related_files/scripts/backup_angular-e23.sh
+++ related_files/scripts/backup_angular-e23.sh
@@ -6,10 +6,10 @@ echo "Copie de sécurité des fichiers relatifs à l'application angular-e23"
 apply_pause "Appuyer sur la touche [Retour] pour continuer..."
 
 source=/var/www/html/d002/angular-e23
-destination=/home/Exercises/angular-e23/related_files/publication
+destination=/home/xd21/angular-e23/related_files/publication
 rm $destination/*.tar.gz
 tar -czvf $destination/pub_angular-e23_`(date +%Y%m%d-%H%M)`.tar.gz $source/
 
-source=/home/Exercises/angular-e23/
+source=/home/xd21/angular-e23/
 destination=/home/Backups/angular-e23_`(date +%Y%m%d-%H%M)`.tar.gz
 tar --exclude=".angular" --exclude=".git" --exclude="node_modules" -czvf $destination $source
